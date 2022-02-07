7f74f43c908e4aa03b45e0fde59b8fddb7c70505
Horus Lugo <horusgoul@gmail.com>
2022-02-07 19:47:17 +0100

    Initial commit

---
+++ app/root.tsx
@@ -0,0 +1,51 @@
+import {
+  Links,
+  LinksFunction,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "remix";
+import type { MetaFunction } from "remix";
+import { useReactNativeStyles } from "./rn-styles";
+import { StyleSheet, View } from "react-native";
+import globalStyles from "./styles/global.css";
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: globalStyles }];
+};
+
+export const meta: MetaFunction = () => {
+  return { title: "New Remix App" };
+};
+
+export default function App() {
+  const stylesElement = useReactNativeStyles();
+
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        {stylesElement}
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <View pointerEvents="box-none" style={styles.appContainer}>
+          <Outlet />
+        </View>
+        <ScrollRestoration />
+        <Scripts />
+        {process.env.NODE_ENV === "development" && <LiveReload />}
+      </body>
+    </html>
+  );
+}
+
+const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1,
+  },
+});
---
+++ app/routes/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text>Hello, world!</Text>
+    </View>
+  );
+}
